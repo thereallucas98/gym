d75ba4bc95c8c179930aa3e8366d643313cb968d
thereallucas98 <lucas1998david@gmail.com>
2024-01-09 07:47:04 -0300

    login and sign up

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@ import { NativeBaseProvider } from 'native-base'
 
 import { Loading } from '~/components/Loading'
 import useLoadFonts from '~/hooks/useLoadFonts'
+import { SignIn } from '~/screens/sign-in'
 import { THEME } from '~/styles/theme'
-import { SignIn } from '~/screens/SignIn'
 
 export default function App() {
   const [fontsLoaded] = useLoadFonts()
