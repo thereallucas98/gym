58a88ba276f17be228755a39bf7c41a39a8ddd88
thereallucas98 <lucas1998david@gmail.com>
2024-01-10 08:58:16 -0300

    profile picture selected

--- src/screens/private/profile.tsx
+++ src/screens/private/profile.tsx
@@ -1,4 +1,15 @@
-import { Center, Heading, ScrollView, Text, VStack } from 'native-base'
+import * as FileSystem from 'expo-file-system'
+import * as ImagePicker from 'expo-image-picker'
+import {
+  Center,
+  Heading,
+  ScrollView,
+  Skeleton,
+  Text,
+  useToast,
+  VStack,
+} from 'native-base'
+import { useState } from 'react'
 import { TouchableOpacity } from 'react-native'
 
 import { Button } from '~/components/button'
@@ -6,19 +17,79 @@ import { ScreenHeader } from '~/components/header/screen-header'
 import { UserPhoto } from '~/components/header/user-photo'
 import { Input } from '~/components/input'
 
+const PHOTO_SIZE = 33
+
 export function Profile() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [userPhoto, setUserPhoto] = useState(
+    'https://github.com/thereallucas98.png',
+  )
+  const toast = useToast()
+
+  async function handleUserPhotoSelected() {
+    try {
+      setIsLoading(true)
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      })
+
+      if (photoSelected.canceled) {
+        return toast.show({
+          title: 'Você cancelou a seleção de imagem.',
+          placement: 'top',
+          bgColor: 'red.500',
+        })
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri,
+        )
+
+        if (photoInfo.exists) {
+          if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+            return toast.show({
+              title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+              placement: 'top',
+              bgColor: 'red.500',
+            })
+          }
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri)
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
 
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={{ uri: 'https://github.com/thereallucas98.png' }}
-            alt="David Lucas"
-            size={33}
-          />
-          <TouchableOpacity>
+          {isLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: userPhoto }}
+              alt="David Lucas"
+              size={PHOTO_SIZE}
+            />
+          )}
+          <TouchableOpacity onPress={handleUserPhotoSelected}>
             <Text
               color="green.500"
               fontWeight={700}
