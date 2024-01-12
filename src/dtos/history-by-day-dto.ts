78a9d8cd94fb78ac0b1a960bf1e32e1a0abdfeb7
thereallucas98 <lucas1998david@gmail.com>
2024-01-12 09:27:21 -0300

    feat: history

---
+++ src/dtos/history-by-day-dto.ts
@@ -0,0 +1,6 @@
+import { HistoryDTO } from './history-dto'
+
+export type HistoryByDayDTO = {
+  title: string
+  data: HistoryDTO[]
+}
--- src/screens/private/history.tsx
+++ src/screens/private/history.tsx
@@ -1,29 +1,52 @@
-import { Heading, SectionList, Text, VStack } from 'native-base'
-import { useEffect, useState } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useFocusEffect } from '@react-navigation/native'
+import { Heading, SectionList, Text, useToast, VStack } from 'native-base'
+import { useCallback, useState } from 'react'
 
 import { ScreenHeader } from '~/components/header/screen-header'
 import { HistoryCard } from '~/components/history-card'
-
-type ExerciseDTO = {
-  title: string
-  data: string[]
-}
+import { Loading } from '~/components/loading'
+import { HistoryByDayDTO } from '~/dtos/history-by-day-dto'
+import { api } from '~/lib/axios'
+import { AppError } from '~/utils/app-error'
 
 export function History() {
-  const [exercises, setExercises] = useState<ExerciseDTO[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [exercises, setExercises] = useState<HistoryByDayDTO[]>([])
 
-  useEffect(() => {
-    setExercises([
-      {
-        title: '26.08.22',
-        data: ['Puxada frontal', 'Remada unilateral'],
-      },
-      {
-        title: '27.08.22',
-        data: ['Puxada frontal'],
-      },
-    ])
-  }, [])
+  const toast = useToast()
+
+  async function fetchHistory() {
+    try {
+      setIsLoading(true)
+      const response = await api.get('/history')
+
+      setExercises(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível carregar os detalhes do exercício'
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchHistory()
+    }, []),
+  )
+
+  if (isLoading) {
+    return <Loading />
+  }
 
   return (
     <VStack flex={1}>
@@ -31,8 +54,8 @@ export function History() {
 
       <SectionList
         sections={exercises}
-        keyExtractor={(item) => item}
-        renderItem={() => <HistoryCard />}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <HistoryCard data={item} />}
         renderSectionHeader={({ section }) => (
           <Heading
             color="gray.200"
