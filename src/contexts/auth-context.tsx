098bb2fa998268031daf59016ad25311df655d5d
thereallucas98 <lucas1998david@gmail.com>
2024-01-17 15:03:56 -0300

    feat: token manager from api axios lib

--- src/contexts/auth-context.tsx
+++ src/contexts/auth-context.tsx
@@ -138,6 +138,14 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     loadUserData()
   }, [])
 
+  useEffect(() => {
+    const subscribe = api.registerInterceptTokenManager(signOut)
+
+    return () => {
+      subscribe()
+    }
+  }, [])
+
   return (
     <AuthContext.Provider
       value={{
